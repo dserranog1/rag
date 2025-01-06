32ee593eac10e43b42b867f3e3e25b2c10d9fd64
Daniel Serrano <dserranog@unal.edu.co>
2025-01-06 11:50:39 -0500

    refactor: retrieve and generate pipeline to langgraph

--- rag_agent_man/main.py
+++ rag_agent_man/main.py
@@ -1,21 +1,43 @@
-from rag_agent_man.vector_store import get_vector_store
-from rag_agent_man.prompt import get_promt
 from langchain_ollama import ChatOllama
+from langchain_core.documents import Document
+from typing_extensions import List, TypedDict
+from langgraph.graph import START, StateGraph
+from rag_agent_man.prompt import get_promt
+from rag_agent_man.vector_store import get_vector_store
 
 
-def retrieve_docs_by_similarity(query):
+class State(TypedDict):
+    question: str
+    context: List[Document]
+    answer: str
+
+
+def get_question(state: State):
+    question = input("Hello! How can I help you? \n")
+    return {"question": question}
+
+
+def retrieve(state: State):
     vector_store = get_vector_store()
-    return vector_store.similarity_search(query, 2)
+    retrieved_docs = vector_store.similarity_search(state["question"], 2)
+    docs_content = "\n\n".join(doc.page_content for doc in retrieved_docs)
+    return {"context": docs_content}
 
+
+def generate(state: State):
+    llm = ChatOllama(model="llama3.1:8b")
+    prompt = get_promt()
+    messages = prompt.invoke(
+        {"question": state["question"], "context": state["context"]}
+    )
+    response = llm.invoke(messages)
+    return {"answer": response.content}
+
+def print_answer(state: State):
+    print(state["answer"])
 
 if __name__ == "__main__":
-    while True:
-        query = input("Hello! How can I help you today?:\n")
-        print("************")
-        retrieved_docs = retrieve_docs_by_similarity(query)
-        docs_content = "\n\n".join(doc.page_content for doc in retrieved_docs)
-        prompt = get_promt()
-        final_prompt = prompt.invoke({"question": query, "context": docs_content})
-        llm = ChatOllama(model="llama3.1:8b")
-        answer = llm.invoke(final_prompt)
-        print(answer.content)
+    graph_builder = StateGraph(State).add_sequence([get_question, retrieve, generate, print_answer])
+    graph_builder.add_edge(START, "get_question")
+    graph = graph_builder.compile()
+    graph.invoke({"question": None})
