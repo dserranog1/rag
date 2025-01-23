defcedd0ac5d784790ddcb9d4cedf54fae863b35
Daniel Serrano <dserranog@unal.edu.co>
2025-01-23 15:31:14 -0500

    feat: slightly alter prompt looking for more concise answers

--- rag_agent_man/prompt.py
+++ rag_agent_man/prompt.py
@@ -1,9 +1,10 @@
 TEMPLATE = """Responde a la pregunta de manera precisa y utilizando únicamente la información que se encuentra en el contexto. 
 Si la información no está disponible, responde: "Lo siento, no lo sé", sin agregar nada.
-No hagas suposiciones ni inventes respuestas. Mantén tus respuestas concisas.
+No hagas suposiciones ni inventes respuestas. Mantén tus respuestas concisas, simples, y no agregues nada que no se encuentre en el contexto.
+De ser posible, utiliza la información del contexto textualmente.
 
-Contexto: {context}
+***Contexto***: {context}
 
-Pregunta: {question}
+***Pregunta***: {question}
 
 Respuesta:"""
